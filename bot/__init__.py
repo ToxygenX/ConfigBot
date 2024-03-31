d1bf722552649f54ef3f0086db5efa3fbd36f34c
CoderX <66735676+ToxygenX@users.noreply.github.com>
2024-03-31 04:24:00 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,7 +4,7 @@ import os
 import time
 from pathlib import Path
 from sys import argv
-from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+import logging
 
 import telethon.utils
 from telethon import TelegramClient, events, functions, types
@@ -15,24 +15,29 @@ ENV = os.environ.get("ENV", True)
 if ENV:
     from bot.BotConfig import Config
     
-LOGS = getLogger("bot")
-TelethonLogger = getLogger("Telethon")
-TelethonLogger.setLevel(WARNING)
+logging.basicConfig(
+    level=logging.INFO,
+    datefmt="%d/%m/%Y %H:%M:%S",
+    format="[%(asctime)s][%(name)s][%(levelname)s] ==> %(message)s",
+    handlers=[logging.StreamHandler(stream=sys.stdout),
+              logging.FileHandler("captionbot.log", mode="a", encoding="utf-8")],)
+
+logging.getLogger("pyrogram").setLevel(logging.INFO)
 
 botx = TelegramClient("thebotx", api_id=Config.API_ID, api_hash=Config.API_HASH)
     
 if Config.BOT_TOKEN is None:
-    LOGS.info("BOT_TOKEN is None. Bot Is Quiting")
+    logging.info("BOT_TOKEN is None. Bot Is Quiting")
     sys.exit(1)
 if Config.API_ID is None:
-    LOGS.info("API_ID Is None.")
+    logging.info("API_ID Is None.")
     sys.exit(1)
 if Config.API_HASH is None:
-    LOGS.info("API_HASH Is None. Exiting...")
+    logging.info("API_HASH Is None. Exiting...")
     sys.exit(1)
 if Config.OWNER_ID is None:
-    LOGS.info("OWNER_ID is None. Please Add Your ID to Continue.")
+    logging.info("OWNER_ID is None. Please Add Your ID to Continue.")
     sys.exit(1)
 if Config.LOG_CHAT is None:
-    LOGS.info("Add LOG_CHAT For This Bot to Work")
+    logging.info("Add LOG_CHAT For This Bot to Work")
     sys.exit(1)
