4ae4c5f2deb6eb5c84097dcf599300f34f8d3764
CoderX <66735676+ToxygenX@users.noreply.github.com>
2024-04-01 22:03:14 +0330

    Update BotConfig.py

--- bot/BotConfig.py
+++ bot/BotConfig.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    API_HASH = os.environ.get("API_HASH", None)
+    API_HASH = os.environ.get("API_HASH", "0c30e3026276a17c3cc4471d7d0883de")
     API_ID = int(os.environ.get("APP_ID", 26109053))
     SESSION = os.environ.get("SESSION", None)
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
